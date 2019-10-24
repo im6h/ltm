03f118360204258e38202ea8ec092ba2cd2f3c83
tjjone98 <tzero1898@gmail.com>
2019-10-24 11:51:59 +0700

    commit
    - xử lý tính năng trả về danh sách user đang online

--- src/main/java/Endpoint.java
+++ src/main/java/Endpoint.java
@@ -12,70 +12,107 @@ import java.util.*;
 public class Endpoint {
 
     private static Set<Session> users = Collections.synchronizedSet(new HashSet<Session>());
-    private static Gson gson = new Gson();
-    // get session and websocket connection
+
+    /* hàm kết nối*/
     @OnOpen
     public void onOpen(Session session){
         users.add(session);
     }
 
-    // handle receive message from client
+    /* hàm nhận tin nhắn từ phía client và gửi trả tin nhắn cho client */
     @OnMessage
     public void onMessage(Session session, String msgJson) throws IOException {
+        Gson gson = new Gson();
         MessageReceive messageReceive = gson.fromJson(msgJson,MessageReceive.class);
+        if (messageReceive.getMessageType() == MessageType.LOGIN){
+            handleLoginMessage(messageReceive,session);
+        }else if (messageReceive.getMessageType() == MessageType.MESSAGE){
+            handleMessageMessage(messageReceive,session);
+        }else if (messageReceive.getMessageType() == MessageType.LISTUSER){
+            handleMessageListUser(messageReceive,session);
+        }
+    }
+    
+    /* chức năng lấy về danh sách user đang online trên hệ thống */
+    private void handleMessageListUser(MessageReceive messageReceive, Session session) {
         MessageResponse messageResponse = new MessageResponse();
-        String content = messageReceive.getContent();
+        Gson gson = new Gson();
         MessageType type = messageReceive.getMessageType();
-        if (type == MessageType.LOGIN){
-            if (session.getUserProperties().get("name") == null){
-                session.getUserProperties().put("name",content);
-                messageResponse.setMessageType(type);
-                messageResponse.setContent(content + " login");
-                String msg = gson.toJson(messageResponse);
-                sendText(msg);
-            }
-        }
-        if (type == MessageType.MESSAGE){
-            messageResponse.setMessageType(type);
-            messageResponse.setContent(session.getUserProperties().get("name") + ":" +content);
-            String msg = gson.toJson(messageResponse);
-            sendText(msg);
-        }
-        if (type == MessageType.LISTUSER){
-            List<String> userOnline = new ArrayList<>();
-            users.forEach(u->{
-                String name = (String) u.getUserProperties().get("name");
-                userOnline.add(name);
-            });
-            messageResponse.setMessageType(type);
-            messageResponse.setContent(gson.toJson(userOnline));
-            String msg = gson.toJson(messageResponse);
-            users.forEach(u->{
-                if (session.equals(u)){
-                    try {
-                        u.getBasicRemote().sendText(msg);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
-
+        String content = gson.toJson(getUserOnline());
+        messageResponse.setMessageType(type);
+        messageResponse.setContent(content);
+        String msg = gson.toJson(messageResponse);
+        sendRequestUser(session,msg);
     }
 
-    // close connect
+    /* chức năng online vào hệ thống */
+    private void handleLoginMessage(MessageReceive messageReceive,Session session) {
+        MessageResponse messageResponse = new MessageResponse();
+        Gson gson = new Gson();
+        String name = messageReceive.getContent();
+        MessageType type = messageReceive.getMessageType();
+        if (session.getUserProperties().get("name")== null){
+            session.getUserProperties().put("name",name);
+            String content = gson.toJson(getUserOnline());
+            messageResponse.setMessageType(type);
+            messageResponse.setContent(content);
+            String msg = gson.toJson(messageResponse);
+            sendMessageText(msg);
+        }
+    }
+
+    /* chức năng gửi tin nhắn cho client */
+    private void handleMessageMessage(MessageReceive messageReceive, Session session) {
+        MessageResponse messageResponse = new MessageResponse();
+        Gson gson = new Gson();
+        MessageType type = messageReceive.getMessageType();
+        String content = messageReceive.getContent();
+        messageResponse.setMessageType(type);
+        messageResponse.setContent(session.getUserProperties().get("name") + ":" +content);
+        String msg = gson.toJson(messageResponse);
+        sendMessageText(msg);
+    }
+
+    /* hàm trả về toàn bộ tên của user đang online*/
+    private List<String> getUserOnline (){
+        List<String> userOnline = new ArrayList<>();
+        users.forEach(u->{
+            String name = (String) u.getUserProperties().get("name");
+            userOnline.add(name);
+        });
+        return userOnline;
+    }
+    private void sendRequestUser(Session session, String msg){
+        users.forEach(u->{
+            if (u.equals(session)){
+                try {
+                    u.getBasicRemote().sendText(msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+     /* đóng kết nối, dĩ nhiên rồi .... */
     @OnClose
     public void onClose(Session session){
         users.remove(session);
+        Gson gson = new Gson();
+        String content = gson.toJson(getUserOnline());
+        MessageResponse messageResponse = new MessageResponse(MessageType.LISTUSER,content);
+        String msg = gson.toJson(messageResponse);
+        sendMessageText(msg);
     }
 
-    // handle error
+    /* xử lý lỗi liên quan khi kết lối ...*/
     @OnError
     public void onError(Throwable throwable){
         throwable.printStackTrace();
     }
 
-    public void sendText(String msg){
+    /* hàm cho phép gửi 1 tin nhắn đến toàn bộ người dùng trong hệ thống */
+    private void sendMessageText(String msg){
         users.forEach(u->{
             try {
                 u.getBasicRemote().sendText(msg);
